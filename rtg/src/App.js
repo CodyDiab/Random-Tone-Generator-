764e27d761690a5649d5fa7bb472c47186487312
Diab <emailcodydiab@gmail.com>
2020-10-16 09:09:42 -0500

    create keys layout

--- rtg/src/App.js
+++ rtg/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import './styles/main.css';
 
+import Keys from "./components/Keys"
+
 function App() {
   return (
     <div className="container">
        <div className="synth">
-
+          <Keys/>
        </div>
 
     </div>

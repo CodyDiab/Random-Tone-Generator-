0aebe20fd4f49f64f29e009a051e2e9948784f50
Diab <emailcodydiab@gmail.com>
2020-10-16 08:28:00 -0500

    set up synth container

---
+++ rtg/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './styles/main.css';
+
+function App() {
+  return (
+    <div className="container">
+       <div className="synth">
+
+       </div>
+
+    </div>
+  );
+}
+
+export default App;
